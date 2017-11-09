445076ce087273697898095b67de6ec532250317
redhaputra <putra.redha@gmail.com>
2017-11-09 13:21:00 +0700

    create users routes

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', controller.findAll);
+
+router.post('/', controller.register);
+
+router.post('/', controller.login);
 
 module.exports = router;
